be9108d9f21d2f1431f9fdc2ca8a8bed8a53311f
phipleg <phipleg@gmx.de>
2016-01-24 19:27:36 +0100

    added powerups

--- buff_buff.js
+++ buff_buff.js
@@ -21,6 +21,7 @@ function game_init() {
     board = new Board();
     player = new Player();
     info = new Info();
+    powerups = new PowerUps();
     gamestart = new GameStart();
     gameover = new GameOver();
     gamepause = new GamePause();
@@ -28,11 +29,10 @@ function game_init() {
 
 }
 
-
 function start_game() {
     if (!is_game_started) {
         is_game_started = true;
-        universe = [player, board, info, background];
+        universe = [player, powerups, board, info, background];
     }
 }
 
@@ -41,7 +41,7 @@ function pause_game() {
         if (!is_game_paused) {
             is_game_paused = true;
             prev_universe = universe;
-            universe = [gamepause, player, board, info, background];
+            universe = [gamepause, player, powerups, board, info, background];
         } else {
             is_game_paused = false;
             universe = prev_universe;
@@ -54,6 +54,14 @@ function escape_game() {
         game_init();
     } else {
         pause_game();
+    }
+}
+
+function check_game_over() {
+    if (is_game_started && !player.alive) {
+        is_game_over = true;
+        is_game_started = false;
+        universe = [gameover, player, powerups, board, info, background];
     }
 }
 
@@ -97,16 +105,12 @@ function on_keyup(e) {
 
 function run() {
     setInterval(function() {
-        for (i = universe.length-1; i >= 0; i--) {
+        for (var i = universe.length-1; i >= 0; i--) {
             object = universe[i];
             object.move();
             object.draw();
         }
-        if (is_game_started && !player.alive) {
-            is_game_over = true;
-            is_game_started = false;
-            universe = [gameover, player, board, info, background];
-        }
+        check_game_over();
     }, 25);
 }
 
@@ -129,14 +133,13 @@ function Player(){
     this.prev_y = this.y;
     this.is_transparent = false;
     this.draw = function(){
-        if (!this.is_transparent) {
-            //c.beginPath();
-            //c.arc(this.x+cw2, this.y+ch2, this.size, this.angle - 0.5*Math.PI, this.angle + 0.5 * Math.PI, false);
-            //c.fillStyle = "yellow";
-            //c.fill();
-        }
+        c.beginPath();
+        c.arc(this.x+cw2, this.y+ch2, this.size, this.angle - 1*Math.PI, this.angle + 1 * Math.PI, false);
+        c.fillStyle = "yellow";
+        c.fill();
     }
     this.move = function(){
+
         if (!this.alive || is_game_paused) {
             return;
         }
@@ -147,8 +150,8 @@ function Player(){
         var vx = Math.cos(this.angle) * this.v; var vy = Math.sin(this.angle) * this.v;
         var new_x = this.x + vx;
         var new_y = this.y + vy;
-        if (this.is_collided(new_x,new_y)) {
-            this.alive = false;
+        this.collision_detection(new_x, new_y);
+        if (!this.alive) {
             return;
         }
         if (board.time % 100 >= 80 ) {
@@ -157,7 +160,6 @@ function Player(){
             this.is_transparent = false;
         }
         if (!this.is_transparent) {
-            //board.add_circle(this.x, this.y, this.size);
             board.add_line(this.prev_x, this.prev_y, this.x, this.y, new_x, new_y, 2* this.size);
         }
         this.prev_x = this.x;
@@ -165,17 +167,18 @@ function Player(){
         this.x = new_x;
         this.y = new_y;
     };
-    this.is_collided = function(new_x,new_y) {
-        var collision = false;
-        var dist = 1.1*this.size;
-        for (t=-1; t<=1; t+=1){
+    this.collision_detection = function(new_x,new_y) {
+        var dist = 1.1 * this.size;
+        for (var t=-1; t<=1; t+=1){
             var beta = this.angle + t * Math.PI/2 * 0.8;
-            var pixel_ok = board.is_empty(new_x + Math.cos(beta)*dist, new_y + Math.sin(beta)*dist);
-            if (!pixel_ok) {
-                collision = true;
+            var sx = new_x + Math.cos(beta) * dist;
+            var sy = new_y + Math.sin(beta) * dist;
+            var hit = !board.is_empty_at(sx, sy);
+            if (hit) {
+                this.alive = false;
             }
+            powerups.pick_from(sx,sy);
         }
-        return collision;
     };
 }
 
@@ -226,12 +229,11 @@ function Board() {
         c.beginPath();
         c.lineWidth = lineWidth;
         c.moveTo(x1+this.w/2,y1+this.h/2);
-        //c.lineTo(x2+this.w/2,y2+this.h/2);
         c.quadraticCurveTo(x2+this.w/2,y2+this.h/2,x3+this.w/2,y3+this.h/2);
         c.strokeStyle = 'red';
         c.stroke();
     };
-    this.is_empty = function(x,y) {
+    this.is_empty_at = function(x,y) {
         var ctx = this.space_ctx;
         var ix = Math.max(0, Math.min(this.w, x+this.w/2));
         var iy = Math.max(0, Math.min(this.h, y+this.h/2));
@@ -246,6 +248,65 @@ function Board() {
     };
 }
 
+function PowerUps() {
+    this.array = [];
+    this.select = function(x,y, other_radius) {
+        var result = [];
+        if (!other_radius) {
+            other_radius = 0;
+        }
+        for (var i=this.array.length-1; i>=0; i--) {
+            var p = this.array[i];
+            var dx = x - p.x;
+            var dy = y - p.y;
+            var dist = Math.sqrt(dx*dx+dy*dy) - p.radius - other_radius;
+            if (dist <= 0) {
+                result.push(p);
+            }
+        }
+        return result;
+    };
+    this.add = function() {
+        for (i=0; i<10; i++) {
+            var p = new PowerUpSpeed();
+            var overlap = this.select(p.x, p.y, p.radius);
+            if (overlap.length == 0) {
+                this.array.push(p);
+                return;
+            }
+        }
+    };
+    this.pick_from = function(x,y) {
+        var result = this.select(x,y);
+        for (var i=result.length-1; i>=0; i--) {
+            var p = result[i];
+            this.array.splice(this.array.indexOf(p),1);
+        }
+    };
+    this.draw = function(){
+        for (var i=0; i<this.array.length; i++) {
+            this.array[i].draw();
+        }
+    };
+    this.move = function(){
+        if (Math.random() < 1.0/(10. + this.array.length*this.array.length)) {
+            this.add();
+        }
+    };
+}
+
+function PowerUpSpeed() {
+    this.img = new Image();
+    this.img.src = "img/Brain-Games-red.png";
+    this.x = (Math.random()-0.5)*board.w;
+    this.y = (Math.random()-0.5)*board.h/2;
+    this.radius = 16;
+    this.draw = function(){
+        c.drawImage(this.img, this.x - this.radius + cw2, this.y - this.radius + ch2, 2*this.radius, 2*this.radius);
+    };
+    this.move = function(){};
+}
+
 function Background() {
     this.draw = function(){
         c.fillStyle = "black";
@@ -256,10 +317,7 @@ function Background() {
 
 
 function Info(){
-    this.img = new Image();
-    this.img.src = "img/Brain-Games-red.png";
     this.draw = function(){
-        c.drawImage(this.img, 200, 16, 32, 32); 
         c.fillStyle = player.alive ? "white" : "grey";
         c.font = "16px pixelfont";
         c.textAlign = "left";
